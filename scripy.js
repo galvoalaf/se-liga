f74ef4a68dd7e814426ea91bfbbd143a3fd549e4
galvoalaf <141366193+galvoalaf@users.noreply.github.com>
2024-12-03 09:17:30 -0300

    ok

---
+++ scripy.js
@@ -0,0 +1,25 @@
+document.getElementById('formulario').addEventListener('submit',function
+    (event){
+        event.preventDefault();
+
+        //obter as notas inseridas pelo usuário
+        let nota1 = parseFloat(document.getElementById('nota1').value);
+        let nota2 = parseFloat(document.getElementById('nota2').value);
+        let nota3 = parseFloat(document.getElementById('nota3').value);
+
+        //calcular a média
+        let media =(nota1 + nota2 + nota3)/ 3;
+
+        //exibir a mensagem de resultado
+        let resultadoDiv = document.getElementById('resultado');
+        if (media>=6.0){
+            resultadoDiv.innerHTML = '<p class="aprovado">Aprovado!Sua média foi: ${media.toFixed(2)}</p>';
+        } else {
+            resultadoDiv.innerHTML = '<p class="recuperaçao">Recoeração. Sua média foi: ${media.toFixed(2)}</p>';
+        }
+    });
+
+
+    Aluno(a):________________________________TURMA:______
+
+    Aluno(a):________________________________TURMA:______
